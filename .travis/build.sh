b9de5d3e1e3f1db82d6f0ca00c455c45b476a815
Ulf Lilleengen <lulf@redhat.com>
2018-08-08 11:23:11 +0200

    Allow successfull PR builds for forks

---
+++ .travis/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Assumes that COMMIT, DOCKER_USER and DOCKER_PASS to be set
+VERSION=${1:-"latest"}
+COMMIT=$2
+REPO=enmasseproject/qdrouterd-base
+
+if [ -n "$TRAVIS_TAG" ]
+then
+    VERSION="$TRAVIS_TAG"
+fi
+
+make || exit 1
+docker build --build-arg version=$VERSION -t $REPO:$COMMIT . || exit 1
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "Pushing $REPO:$COMMIT"
+    docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
+    docker push $REPO:$COMMIT || exit 1
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        echo "Pushing $REPO:$VERSION"
+        docker tag $REPO:$COMMIT $REPO:$VERSION || exit 1
+        docker push $REPO:$VERSION || exit 1
+    fi
+fi
