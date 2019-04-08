b1aa3e88e60f062c33df83c4d007d3b56363aecc
Ulf Lilleengen <lulf@redhat.com>
2019-04-08 13:12:46 +0200

    Add patch for DISPATCH-1287

--- .travis/build.sh
+++ .travis/build.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Pushing $REPO:$COMMIT"
     docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
     docker push $REPO:$COMMIT || exit 1
-    if [ "$TRAVIS_BRANCH" == "master" ]; then
+    if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "1.5.0-dispatch-1273-1287" ]; then
         echo "Pushing $REPO:$VERSION"
         docker tag $REPO:$COMMIT $REPO:$VERSION || exit 1
         docker push $REPO:$VERSION || exit 1
