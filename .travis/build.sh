e44e9c3debb0a712c4c0404e9e63ac6266200a10
Ulf Lilleengen <lulf@redhat.com>
2019-04-27 19:23:32 +0200

    Login to registry

--- .travis/build.sh
+++ .travis/build.sh
@@ -15,7 +15,7 @@ docker build --build-arg version=$VERSION -t $REPO:$COMMIT . || exit 1
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Pushing $REPO:$COMMIT"
-    docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
+    docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY || exit 1
     docker push $REPO:$COMMIT || exit 1
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         echo "Pushing $REPO:$VERSION"
